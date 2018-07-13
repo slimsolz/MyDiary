b897fe59ac057de73ebafaf1c725e895ce83ee4f
Odumah Solomon <odumahs@gmail.com>
2018-07-13 04:56:11 +0100

    Install express

---
+++ server/app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import index from './routes/index';
+
+const app = express();
+
+// API Docs
+const swaggerDocument = YAML.load(`${process.cwd()}/server/swagger.yaml`);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// port
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+// log request to console
+app.use(logger('dev'));
+
+// parse incoming request data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api/v1', index);
+
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
+
+export default app;
---
+++ server/routes/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router();
+
+// homepage
+router.get('/', (req, res, next) => {
+  res.json({
+    status: 'success',
+    message: 'Welcome to my diary app'
+  });
+});
