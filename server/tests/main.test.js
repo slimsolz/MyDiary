7351a7b929490e0baf851f2d34a67956419e2d10
Odumah Solomon <odumahs@gmail.com>
2018-07-13 07:18:45 +0100

    Add test file for POST /

---
+++ server/tests/main.test.js
@@ -0,0 +1,19 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../app';
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+// homepage
+describe('GET /', () => {
+  it('should return 200 and a welcome message', (done) => {
+    chai.request(app)
+      .get('/api/v1/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.be.equal('Welcome to my diary app');
+        done();
+      });
+  });
+});
