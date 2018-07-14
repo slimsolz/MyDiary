375e8c74d0786fbbb46c77ff7848040ac2a513a6
Odumah Solomon <odumahs@gmail.com>
2018-07-14 15:57:05 +0100

    Add POST /entries route

---
+++ server/controllers/entryController.js
@@ -0,0 +1,32 @@
+import db from '../models/dummy-db';
+
+export default class EntryController {
+  static addEntry(req, res) {
+    const {
+      title, category, image, story
+    } = req.body;
+
+    const newEntry = {
+      id: db.entry[db.entry.length - 1].id + 1,
+      title,
+      category,
+      image,
+      story
+    };
+
+    const entryfound = db.entry.find(entry => entry.title.toLowerCase() === title.toLowerCase());
+    if (entryfound) {
+      return res.status(409).json({
+        status: 'error',
+        message: 'Entry already exists'
+      });
+    }
+
+    db.entry.push(newEntry);
+    return res.status(201).json({
+      status: 'success',
+      message: 'Entry saves successfully',
+      entry: newEntry
+    });
+  }
+}
--- server/models/dummy-db.js
+++ server/models/dummy-db.js
@@ -11,4 +11,19 @@ db.users = [{
   password: 'user2password'
 }];
 
+db.entry = [{
+  id: 1,
+  title: 'Beautiful Mother',
+  category: 'Mom',
+  image: 'beautifulmom.jpg',
+  story: 'For so long, I wished for the day. The day that our love would find its way. From my heart and into your soul, The feeling so strong, I had no control. When then that day came, when I found you again, I vowed never to make the same mistake. I knew I would never let you go, For my life is now complete in a way I cannot show. For eternity I will spend making you believe, You are the sole reason that I breathe. My life is yours, my hopes and desires too. Until my dying day, my heart is reserved just for you. You are everything I could ever need and more, More than I deserve or would dare wish for. You are my baby, my angel, my dream girl I\'m thankful every day that you are my whole world. For the time I spend with you, my heart truly sings My one, My only, My EVERYTHING.'
+},
+{
+  id: 2,
+  title: 'How do I Live Without You',
+  category: 'Love',
+  image: 'heart.img',
+  story: 'For so long, I wished for the day. The day that our love would find its way. From my heart and into your soul, The feeling so strong, I had no control. When then that day came, when I found you again, I vowed never to make the same mistake. I knew I would never let you go, For my life is now complete in a way I cannot show. For eternity I will spend making you believe, You are the sole reason that I breathe. My life is yours, my hopes and desires too. Until my dying day, my heart is reserved just for you. You are everything I could ever need and more, More than I deserve or would dare wish for. You are my baby, my angel, my dream girl I\'m thankful every day that you are my whole world. For the time I spend with you, my heart truly sings My one, My only, My EVERYTHING.'
+}];
+
 export default db;
