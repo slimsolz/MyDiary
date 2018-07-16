a77e353519693be47c8db96b9bff5aa88b3e582f
Odumah Solomon <odumahs@gmail.com>
2018-07-16 14:50:06 +0100

    Add middleware validator for user profile

--- server/routes/index.js
+++ server/routes/index.js
@@ -16,7 +16,7 @@ router.get('/', (req, res, next) => {
 /* user */
 router.post('/auth/signup', Middleware.validateUser, UserController.signup);
 router.post('/auth/signin', Middleware.validateUser, UserController.signin);
-router.put('/user/account/:id', UserController.updateProfile);
+router.put('/user/account/:id', Middleware.validateParams, Middleware.validateProfie, UserController.updateProfile);
 
 /* Entry */
 router.post('/entries', Middleware.validateEntry, EntryController.addEntry);
