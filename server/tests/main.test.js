e2a5338de6cfaf1705741e2e4d33ee2d27e94e09
Odumah Solomon <odumahs@gmail.com>
2018-07-20 07:07:34 +0100

    Write tests for 404 routes

--- server/tests/main.test.js
+++ server/tests/main.test.js
@@ -17,3 +17,51 @@ describe('GET /', () => {
       });
   });
 });
+
+describe('POST /', () => {
+  it('should returon 404 and an error message', (done) => {
+    chai.request(app)
+      .post('/api/v1/xyz')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        expect(res.body.message).to.be.equal('404 Page not found');
+        done();
+      });
+  });
+});
+
+describe('PUT /', () => {
+  it('should returon 404 and an error message', (done) => {
+    chai.request(app)
+      .put('/api/v1/xyz')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        expect(res.body.message).to.be.equal('404 Page not found');
+        done();
+      });
+  });
+});
+
+describe('DELETE /', () => {
+  it('should returon 404 and an error message', (done) => {
+    chai.request(app)
+      .delete('/api/v1/xyz')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        expect(res.body.message).to.be.equal('404 Page not found');
+        done();
+      });
+  });
+});
+
+describe('GET /', () => {
+  it('should returon 404 and an error message', (done) => {
+    chai.request(app)
+      .get('/api/v1/xyz')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        expect(res.body.message).to.be.equal('404 Page not found');
+        done();
+      });
+  });
+});
