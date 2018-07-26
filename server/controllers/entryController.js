e881dce724dca4a0c1244276d4770e4986a14ad8
Odumah Solomon <odumahs@gmail.com>
2018-07-26 13:16:54 +0100

    Add controller for DELETE /entries/:entryid route

--- server/controllers/entryController.js
+++ server/controllers/entryController.js
@@ -60,8 +60,8 @@ export default class EntryController {
       values: [entryId, userId]
     };
 
-    client.query(findEntryQuery, (err, entryfound) => {
-      if (entryfound.rowCount === 0) {
+    client.query(findEntryQuery, (err, entryFound) => {
+      if (entryFound.rowCount === 0) {
         return res.status(404).json({
           status: 'error',
           message: 'Entry not found'
@@ -80,6 +80,36 @@ export default class EntryController {
             image: updatedEntry.rows[0].entry,
             story: updatedEntry.rows[0].story
           }
+        });
+      });
+    });
+  }
+
+  static deleteEntry(req, res) {
+    const client = new Client(connectionString);
+    client.connect();
+    const entryId = parseInt(req.params.id, 10);
+    const { userId } = req;
+
+    const findEntryQuery = {
+      text: 'SELECT * FROM entries WHERE id = $1 AND userId = $2',
+      values: [entryId, userId]
+    };
+
+    client.query(findEntryQuery, (err, entryFound) => {
+      if (entryFound.rowCount === 0) {
+        return res.status(404).json({
+          status: 'error',
+          message: 'Entry not found'
+        });
+      }
+
+      const deleteEntryQuery = `DELETE FROM entries WHERE userId = ${userId} AND id = ${entryId}`;
+      client.query(deleteEntryQuery, (error, result) => {
+        client.end();
+        return res.status(200).json({
+          status: 'success',
+          message: `${result.rowCount} entry deleted`
         });
       });
     });
